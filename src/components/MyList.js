c4c08378ee20a6e141b6dea8505fed234bc584e8
nnelimarkka <nikkeneli@gmail.com>
2021-11-10 16:15:48 +0200

    v 1.0.4

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,11 +1,14 @@
 const MyList = (props) => {
     let itemArray = Array.from(props.items);
+    const mystyle = {
+        textDecoration: "line-through"
+      };
     return (
         <div>
           <h1>{props.header}</h1>
           <ol>
               {itemArray.map((item) => (
-                <li className={item.clicked ? 'clicked' :''} key={item.id} onClick={() => props.updateItem(item.id)}>{item.text}</li>
+                <li style={item.clicked ? mystyle :{}} key={item.id} onClick={() => props.updateItem(item.id)}>{item.text}</li>
             ))}</ol>  
         </div>
     )
